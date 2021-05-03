6f86dae00c65531b35f3027a94731010df8a3997
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-05-03 18:55:34 +0200

    Test triggering unrelated change with open PR

--- frontend/test/metabase/scenarios/visualizations/drillthroughs/chart_drill.cy.spec.js
+++ frontend/test/metabase/scenarios/visualizations/drillthroughs/chart_drill.cy.spec.js
@@ -20,7 +20,7 @@ const {
 const { DATA_GROUP } = USER_GROUPS;
 
 describe("scenarios > visualizations > drillthroughs > chart drill", () => {
-  // Test triggering unrelated change
+  // Test triggering unrelated change this time when PR was already opened
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
